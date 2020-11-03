48d6d32ca27a2edbdd65d561d44f1bbb7e81696b
Nicolás Bueno <nicolasb11@hotmail.com>
2020-11-03 18:23:59 -0500

    Correcting path with os.path.sep and some cleaning issues related to absent columns

---
+++ decline_rate.py
@@ -0,0 +1,21 @@
+fig = plt.figure()
+ax = plt.axes()
+
+group = df_dict['2019'].groupby('contrato').sum()
+campo = "ABANICO"
+data = group.loc[campo,"enero":"diciembre"].T
+x = pd.period_range('2019-01', periods=12, freq='M')
+x_temp = np.arange(0,12)
+ax.plot(x-x[0], data.loc[:],'o', color = 'black')
+    
+arps_start = "enero"
+arps_b = 0.5
+arps_Di = 0.04
+arps_prediction = np.zeros(12);
+arps_prediction = data[arps_start] / pow( 1 + arps_b * arps_Di * x_temp, 1/arps_b)
+
+ax.plot(x-x[0], arps_prediction,'--', color = 'blue');
+print(x)
+print(x-x[0])
+
+#https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
--- utils/cleanData.py
+++ utils/cleanData.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 from glob import glob
-
+import os
 class cleanData:
     def __init__(self):
         pass
-    def lss(self, expr = './data/*.*'):
+    def lss(self, expr = f'.{os.path.sep}data{os.path.sep}*.*'):
         return glob(expr)
 
     def bringLastMonth(self, filenames:list) -> list:
@@ -17,7 +17,7 @@ class cleanData:
                     "nov":11, "dic":12}
         i = 0
         for file in filenames:
-            file = file.replace("./data/","").replace(".xlsx","")
+            file = file.replace(f".{os.path.sep}data{os.path.sep}","").replace(".xlsx","")
             if "2020" not in file:
                 filenames_clean.append(file)
             else:
@@ -38,7 +38,8 @@ class cleanData:
 
         # For 2020 remove the month in the name
         for year in years_files:
-            file_dir = f"./data/{year}.xlsx"
+            file_dir = f".{os.path.sep}data{os.path.sep}{year}.xlsx"
+            #file_dir = f"{year}.xlsx"
             if "2020" in year:
                 df_dict['2020']= pd.read_excel(file_dir)
             else:
@@ -50,7 +51,8 @@ class cleanData:
             df_dict[i]=df_dict[i].reset_index(drop=True) #add dpto back as a col
             df_dict[i].columns=df_dict[i].iloc[0].str.lower() #set new first row as cols names
             df_dict[i]=df_dict[i].drop(df_dict[i].index[0]) #drop row in the df with cols names
-            df_dict[i]= df_dict[i].drop(df_dict[i][df_dict[i]["campo"].isnull() & df_dict[i]["operadora"].isnull() & df_dict[i]["municipio"].isnull()].index)#Eliminate the rows where the value of 
+            if ("empresa" in df_dict[i]): df_dict[i].rename(columns={"empresa": "operadora"}, inplace=True)
+            df_dict[i]= df_dict[i].drop(df_dict[i][df_dict[i]["campo"].isnull() & df_dict[i]["operadora"].isnull() & df_dict[i]["departamento"].isnull() ].index)#Eliminate the rows where the value of 
             #the field, operator and municipality is null because it  would not be possible to determine these data with the 
             #remaining information.
 
