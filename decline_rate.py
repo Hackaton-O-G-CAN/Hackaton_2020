bfdb68f2a06c53b694c065ac56d8dc6cb23461b0
Nicolás Bueno <nicolasb11@hotmail.com>
2020-11-06 14:43:07 -0500

    Grouping and extracting production information across DataFrames. Still not sure how to use multi-index to facilitate data extraction

--- decline_rate.py
+++ decline_rate.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 from glob import glob
+
 import matplotlib.pyplot as plt
+
 from utils import downloadData
 from utils import dataProc
 import pickle
@@ -22,7 +24,7 @@ with open('df_dict.pickle', 'rb') as handle:
 fig = plt.figure()
 ax = plt.axes()
 year = '2019'
-campo = "ABANICO"
+campo = "abanico"
 date_0 = pd.to_datetime(f"01 of Jan,{year}")
 print(date_0)
 #x2 = date_0 + pd.to_timedelta(np.arange(12), 'M')
@@ -36,15 +38,15 @@ x_temp = np.arange(0,12)
 group = df_dict[year].groupby('contrato').sum()
 data = group.loc[campo,"enero":"diciembre"].T
 
-ax.plot(x, data.loc[:],'o', color = 'black')
-    
+ax.plot(x_temp, data.loc[:],'o', color = 'black')
+
 arps_start = "enero"
 arps_b = 0.5
 arps_Di = 0.04
 arps_prediction = np.zeros(12);
 arps_prediction = data[arps_start] / pow( 1 + arps_b * arps_Di * x_temp, 1/arps_b)
 
-ax.plot(x-x[0], arps_prediction,'--', color = 'blue');
+ax.plot(x_temp, arps_prediction,'--', color = 'blue');
 print(x)
 print(x-x[0])
 
